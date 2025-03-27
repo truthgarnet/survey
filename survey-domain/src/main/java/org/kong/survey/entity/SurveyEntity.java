e78ea28b27965eae82d932752227e1581c278ba9
truthgarnet <57488270+truthgarnet@users.noreply.github.com>
2025-03-27 23:40:31 +0900

    chore: add AuditingEntityListener - @CreatedDate - @LastModifiedDate

--- survey-domain/src/main/java/org/kong/survey/entity/SurveyEntity.java
+++ survey-domain/src/main/java/org/kong/survey/entity/SurveyEntity.java
@@ -2,7 +2,9 @@ package org.kong.survey.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -10,31 +12,32 @@ import java.time.LocalDateTime;
 @Table(name = "TB_SURVEY")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class SurveyEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer surveyId;
 
     private String surveyTitle;
 
     private String surveyVersion;
 
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdDate;
 
+    @LastModifiedDate
     private LocalDateTime updatedDate;
 
     private Boolean usedYn;
 
 
     @Builder
-    public SurveyEntity(Integer surveyId, String surveyTitle, String surveyVersion,
-                  LocalDateTime createdDate, LocalDateTime updatedDate, Boolean usedYn) {
+    public SurveyEntity(Integer surveyId, String surveyTitle, String surveyVersion, Boolean usedYn) {
         this.surveyId = surveyId;
         this.surveyTitle = surveyTitle;
         this.surveyVersion = surveyVersion;
-        this.createdDate = createdDate;
-        this.updatedDate = updatedDate;
         this.usedYn = usedYn;
     }
 
