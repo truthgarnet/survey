6b096a036f716cd0704f7b85f30d40bd9914630e
truthgarnet <dmstj105@gmail.com>
2025-05-29 00:06:53 +0900

    refactor: fix typo

--- survey-common/src/main/java/org/kong/exception/ErrorCode.java
+++ survey-common/src/main/java/org/kong/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
   USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "U001", 101, "사용자를 찾을 수 없습니다."),
-  LOGIN_FAILD(HttpStatus.BAD_REQUEST, "U002", 102, "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요."),
+  LOGIN_FAILED(HttpStatus.BAD_REQUEST, "U002", 102, "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요."),
   ADMIN_NOT_FOUND(HttpStatus.BAD_REQUEST, "A001", 101, "관리자를 찾을 수 없습니다."),
   SERVICE_NOT_FOUND(HttpStatus.BAD_REQUEST, "S001", 101, "설문지를 찾을 수 없습니다."),
   QUESTION_NOT_FOUND(HttpStatus.BAD_REQUEST, "Q001", 101, "질문지를 찾을 수 없습니다."),
