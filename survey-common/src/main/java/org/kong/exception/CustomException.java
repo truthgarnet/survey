bead6c901a90e51b8211d63f9654518083e29023
truthgarnet <dmstj105@gmail.com>
2025-05-29 00:09:59 +0900

    refactor: constructor add Objects.requireNonNull - 문제를 빨리 발견하게 도와줌: 방어 코딩

--- survey-common/src/main/java/org/kong/exception/CustomException.java
+++ survey-common/src/main/java/org/kong/exception/CustomException.java
@@ -1,12 +1,23 @@
 package org.kong.exception;
 
-import lombok.AllArgsConstructor;
+import java.util.Objects;
+
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class CustomException extends RuntimeException{
 
     private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(Objects.requireNonNull(errorCode, "ErrorCode must not be null").getMsg());
+        this.errorCode = errorCode;
+    }
+
+    // 외부 시스템 예외
+    public CustomException(ErrorCode errorCode, Throwable cause) {
+        super(Objects.requireNonNull(errorCode, "ErrorCode must not be null").getMsg(), cause);
+        this.errorCode = errorCode;
+    }
 
 }
